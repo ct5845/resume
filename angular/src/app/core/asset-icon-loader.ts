5ebc33bae399374bea20a7ab509f1cd00e29b17b
Christopher Turner <cturner@alterian.com>
2024-10-01 22:29:07 +0100

    New Version of Resume, Picked Svelte

--- angular/src/app/core/asset-icon-loader.ts
+++
@@ -1,8 +0,0 @@
-import { provideNgIconLoader } from "@ng-icons/core";
-import { inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-
-export const assetIconLoader = provideNgIconLoader(name => {
-  const http = inject(HttpClient);
-  return http.get(`/assets/${name}.svg`, { responseType: 'text' });
-});
